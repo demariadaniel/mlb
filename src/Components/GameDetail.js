2d6474a8c585fb3156269344f8721a00f6e6eb74
demariadaniel <demaria.daniel@gmail.com>
2018-02-14 19:42:27 -0500

    Basic Routing

--- src/Components/GameDetail.js
+++ src/Components/GameDetail.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Link, Route, Switch} from 'react-router-dom';
 
 class GameDetail extends Component {
   render(){
     const game = this.props.games[parseInt(this.props.match.params.id)];
     console.log(game)
     return(
-      <div>
-
+      <div className="games container">
+        <div className="row">
+          {/* If game is undefined, show loading message */}
+          {game ? 
+          <div className="gameDetail col-12">
+            <div className="row">
+              {game.home_name_abbrev}
+            </div>
+            <div className="row">
+              {game.away_name_abbrev}
+            </div>
+            <Link to="/" className="link">
+              <button className="btn btn-primary">
+                Home
+              </button>
+            </Link>
+          </div>
+          :
+          <h2> Loading... </h2>}
+        </div>
       </div>
     )
   }
