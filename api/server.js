2b64c6c93664a7cc69982922f2fdaeca29559eb8
demariadaniel <dandemaria@MBPe15C02R5YT9FVH3.local>
2018-02-15 14:30:42 -0500

    Server Port

--- api/server.js
+++ api/server.js
@@ -15,8 +15,8 @@ app.use((req, res, next) => {
   
 app.use(express.static(__dirname + './../build'))
 
-app.listen(8080, ()=>{
-    console.log('listening on port 8080')
+app.listen(process.env.PORT || 8080, ()=>{
+    console.log('server listening')
 })
 
 app.get('/games/:day/:month/:year', (req, res)=>{ 
